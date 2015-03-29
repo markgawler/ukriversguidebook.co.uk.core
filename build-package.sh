821b712496f52fa7c3ac122e239633bac358b568
Mark Gawler <markg@wler.co.uk>
2015-03-29 16:12:37 +0100

    Modify system plugin to use language file.

--- build-package.sh
+++ build-package.sh
@@ -8,8 +8,13 @@ cd com_ukrgb
 zip -qr ../com_ukrgb .
 
 cd ..
-echo "Plugin, Creating Zip..."
+echo "Sys Plugin, Creating Zip..."
 cd plg_ukrgbsystem
 zip -qr ../plg_ukrgbsystem .
+
+cd ..
+echo "Content Plugin, Creating Zip..."
+cd plg_ukrgbcontent
+zip -qr ../plg_ukrgbcontent .
 
 echo "Done."
