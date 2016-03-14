ce934376a3a860560b626771efe48b00d0f322b6
mrfg <markg@wler.co.uk>
2016-03-14 22:19:23 +0000

    Basic routing

--- com_ukrgb/site/router.php
+++ com_ukrgb/site/router.php
@@ -38,12 +38,6 @@ class UkrgbRouter extends JComponentRouterBase
 		else
 		{
 			$menuItem = $this->menu->getItem($query['Itemid']);
-			//echo "<br>-------<br>";
-			//var_dump($menuItem);
-			//echo "<br>-------<br>";
-			//var_dump($menuItem->route);
-			//echo "<br>-------<br>";
-				
 			$menuItemGiven = true;
 		}
 
@@ -96,13 +90,7 @@ class UkrgbRouter extends JComponentRouterBase
 			}
 			$segments[] = $query['id'];
 			unset($query['id']);
-			//$p = '';
-			//foreach ($segments as $s)
-			//{
-			//	$p = $p . $s . '@'; 
-			//}
-			
-			//error_log($p);
+
 		}		
 
 		return $segments;
@@ -119,186 +107,25 @@ class UkrgbRouter extends JComponentRouterBase
 	 */
 	public function parse(&$segments)
 	{
-		//var_dump($segments);
-		//die('parse');
-		$total = count($segments);
 		$vars = array();
-
-		for ($i = 0; $i < $total; $i++)
-		{
-			$segments[$i] = preg_replace('/-/', ':', $segments[$i], 1);
-		}
-
-		// Get the active menu item.
-		$item = $this->menu->getActive();
-		$params = JComponentHelper::getParams('com_ukrgb');
-		$advanced = $params->get('sef_advanced_link', 0);
-		$db = JFactory::getDbo();
-
-		// Count route segments
+		$app = JFactory::getApplication();
+		$menu = $app->getMenu();
+		$item = $menu->getActive();
+		// Count segments
 		$count = count($segments);
-
-		/*
-		 * Standard routing for articles.  If we don't pick up an Itemid then we get the view from the segments
-		 * the first segment is the view and the last segment is the id of the article or category.
-		 */
-		if (!isset($item))
+		// Handle View and Identifier
+		switch ($item->query['view'])
 		{
-			$vars['view'] = $segments[0];
-			$vars['id'] = $segments[$count - 1];
-
-			return $vars;
-		}
-
-		/*
-		 * If there is only one segment, then it points to either an article or a category.
-		 * We test it first to see if it is a category.  If the id and alias match a category,
-		 * then we assume it is a category.  If they don't we assume it is an article
-		 */
-		if ($count == 1)
-		{
-			// We check to see if an alias is given.  If not, we assume it is an article
-			if (strpos($segments[0], ':') === false)
-			{
+			case 'events':
+				$id = explode(':', $segments[$count-1]);
+				$vars['id'] = (int) $id[0];
 				$vars['view'] = 'event';
-				$vars['id'] = (int) $segments[0];
-
-				return $vars;
-			}
-
-			list($id, $alias) = explode(':', $segments[0], 2);
-
-			// First we check if it is a category
-			$category = JCategories::getInstance('com_ukrgb')->get($id);
-
-			if ($category && $category->alias == $alias)
-			{
-				$vars['view'] = 'category';
-				$vars['id'] = $id;
-
-				return $vars;
-			}
-			else
-			{
-				$query = $db->getQuery(true)
-					->select($db->quoteName(array('alias', 'catid')))
-					->from($db->quoteName('#__ukrgb_events'))
-					->where($db->quoteName('id') . ' = ' . (int) $id);
-				$db->setQuery($query);
-				$article = $db->loadObject();
-
-				if ($article)
-				{
-					if ($article->alias == $alias)
-					{
-						$vars['view'] = 'event';
-						$vars['catid'] = (int) $article->catid;
-						$vars['id'] = (int) $id;
-
-						return $vars;
-					}
-				}
-			}
+				break;
 		}
-
-		/*
-		 * If there was more than one segment, then we can determine where the URL points to
-		 * because the first segment will have the target category id prepended to it.  If the
-		 * last segment has a number prepended, it is an article, otherwise, it is a category.
-		 */
-		if (!$advanced)
-		{
-			$cat_id = (int) $segments[0];
-
-			$article_id = (int) $segments[$count - 1];
-
-			if ($article_id > 0)
-			{
-				$vars['view'] = 'event';
-				$vars['catid'] = $cat_id;
-				$vars['id'] = $article_id;
-			}
-			else
-			{
-				$vars['view'] = 'category';
-				$vars['id'] = $cat_id;
-			}
-
-			return $vars;
-		}
-
-		// We get the category id from the menu item and search from there
-		$id = $item->query['id'];
-		$category = JCategories::getInstance('com_ukrgb')->get($id);
-
-		if (!$category)
-		{
-			JError::raiseError(404, JText::_('COM_CONTENT_ERROR_PARENT_CATEGORY_NOT_FOUND'));
-
-			return $vars;
-		}
-
-		$categories = $category->getChildren();
-		$vars['catid'] = $id;
-		$vars['id'] = $id;
-		$found = 0;
-
-		foreach ($segments as $segment)
-		{
-			$segment = str_replace(':', '-', $segment);
-
-			foreach ($categories as $category)
-			{
-				if ($category->alias == $segment)
-				{
-					$vars['id'] = $category->id;
-					$vars['catid'] = $category->id;
-					$vars['view'] = 'category';
-					$categories = $category->getChildren();
-					$found = 1;
-					break;
-				}
-			}
-
-			if ($found == 0)
-			{
-				if ($advanced)
-				{
-					$db = JFactory::getDbo();
-					$query = $db->getQuery(true)
-						->select($db->quoteName('id'))
-						->from('#__ukrgb_events')
-						->where($db->quoteName('catid') . ' = ' . (int) $vars['catid'])
-						->where($db->quoteName('alias') . ' = ' . $db->quote($segment));
-					$db->setQuery($query);
-					$cid = $db->loadResult();
-				}
-				else
-				{
-					$cid = $segment;
-				}
-
-				$vars['id'] = $cid;
-
-				if ($item->query['view'] == 'archive' && $count != 1)
-				{
-					$vars['year'] = $count >= 2 ? $segments[$count - 2] : null;
-					$vars['month'] = $segments[$count - 1];
-					$vars['view'] = 'archive';
-				}
-				else
-				{
-					$vars['view'] = 'article';
-				}
-			}
-
-			$found = 0;
-		}
-
 		return $vars;
+		
 	}
 }
-
 /**
  * UKRGB router functions
  *
