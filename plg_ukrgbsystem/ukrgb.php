b89ec9b9a19ff7b37c044cde93b91871c4769582
Mark Gawler <markg@wler.co.uk>
2016-11-09 19:20:41 +0000

    Remove Paypal code from plugin

--- plg_ukrgbsystem/ukrgb.php
+++ plg_ukrgbsystem/ukrgb.php
@@ -13,8 +13,8 @@ jimport( 'joomla.plugin.plugin' );
 
 class plgSystemUkrgb extends JPlugin {
 	/**
-	 * Plugin to add the PayPal donate button to forum pages 
-	 *
+	 * Plugin to prompt email change and load AWS library
+	 * 
 	 */
 	protected $autoloadLanguage = true;
 	
@@ -33,7 +33,6 @@ class plgSystemUkrgb extends JPlugin {
 	**/
 	public function onBeforeRender()
 	{	
-		
 		$app = JFactory::getApplication();
 		if ($app->isSite()){
 			
@@ -43,72 +42,6 @@ class plgSystemUkrgb extends JPlugin {
 			{					
 				$app->enqueueMessage(JText::_('PLG_SYSTEM_UKRGB_UPDATE_EMAIL').' <a href="'. JRoute::_('index.php?option=com_users&lang=en&layout=edit&view=profile').'">Update Profile</a>');
 			}
-			
-			
-			// Display PayPal button
-			$active_menu = $app->getMenu()->getActive();
-			
-			if (!is_null($active_menu) && $active_menu->id == $this->params->get('forummenuitem')){
-				$uri=JFactory::getURI();
-				if ($uri->hasVar("f") && !$uri->hasVar("p")){
-					$forum = $uri->getVar("f");
-					$selected_forums = $this->params->get('selected_forums');
-					
-					if (is_array($selected_forums)) {
-						if (in_array($forum, $selected_forums)){
-							jimport('joomla.application.component.helper');
-							$config = JComponentHelper::getParams('com_ukrgb');
-							if ($config->get('sandbox')){
-								$paypal_url = "https://www.sandbox.paypal.com/cgi-bin/webscr";
-								$hosted_button_id = $config->get('sandbox_hosted_button_id');
-							}else{
-								$paypal_url = "https://www.paypal.com/cgi-bin/webscr";
-								$hosted_button_id = $config->get('hosted_button_id');
-							}
-							$item_name = $forum.':UK Rivers Controbution';
-							$script = <<<SCRIPT
-jQuery(document).ready(function(){jQuery("#paypal-box").append('<form action="{$paypal_url}" method="post">\
-<input type="hidden" name="cmd" value="_s-xclick">\
-<input type="hidden" name="hosted_button_id" value="{$hosted_button_id}">\
-<input type="hidden" name="custom" value="UK Rivers Guidebook">\
-<input type="hidden" name="item_name" value="{$item_name}">\
-<input type="image" src="https://www.paypal.com/en_GB/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal">\
-<img alt="" border="0" src="https://www.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1">\
-</form>')});
-SCRIPT;
-  							JFactory::GetDocument()->addScriptDeclaration($script);
-						}
-					}
-				}
-			}
-		}
-	}
-
-	/**
-	 * Plugin to remove sh040 canonical link, leaving only the phpBB link.
-	 */
-	public function onAfterRender()
-	{
-		$app = JFactory::getApplication();
-		if ($app->isSite()){
-			$active_menu = $app->getMenu()->getActive();
-			if (!is_null($active_menu) && $active_menu->id == $this->params->get('forummenuitem')){
-				
-				$page = $app->getBody();
-				$canonicalPattern = '#\<link[^>]+rel\s*=\s*"canonical"[^>]+\>#siU';	
-				$matches = array();
-				$canonicalCount = preg_match_all($canonicalPattern, $page, $matches);
-				
-				foreach ($matches[0] as $match){
-					// delete the non phpBB canonical
-					if (strpos($match, '/view,plugin/') !== false) {
-						$page = str_replace($match, '' , $page);
-					}
-				}
-				// remove extraneous /view,plugin/
-				$page = str_replace('/view,plugin/', '/' , $page);
-				$app->setBody($page);
-			}	
 		}
 	}
 }
