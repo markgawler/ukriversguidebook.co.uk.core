f761246773b5124e34ecf352d545612c07d4f083
Mark Gawler <markg@wler.co.uk>
2017-07-01 16:54:05 +0100

    Add basic module for embedding videos

--- mod_ukrgbnestcam/tmpl/default.php
+++ mod_ukrgbnestcam/tmpl/default.php
@@ -1,7 +1,5 @@
 <?php 
 // No direct access
-// https://video.nest.com/embedded/live/3jXMLtff89
-//  webkitallowfullscreen mozallowfullscreen
 defined('_JEXEC') or die; ?>
 <div class="flex-video widescreen">
 <iframe allowfullscreen src="<?php echo $url ?>" frameborder="0" width="1024" height="576"></iframe>
