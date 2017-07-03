fd51e9a4b4da84a59cad62c2dc6346394cf80c0d
Mark Gawler <markg@wler.co.uk>
2017-07-03 20:22:57 +0100

    Add basic module for embedding videos, add acknowledgment link

--- mod_ukrgbnestcam/mod_ukrgbnestcam.php
+++ mod_ukrgbnestcam/mod_ukrgbnestcam.php
@@ -10,9 +10,12 @@
 // No direct access
 defined('_JEXEC') or die;
 $layout = $params->get('layout', 'default');
-$url = $params->get('Srcurl',null);
 $id = $app->input->get('id',0,'int');
 $allowedId = $params->get('Articleid', '0');
 if ($allowedId == 0 or $allowedId == $id){
+	$url = $params->get('Srcurl',null);
+	$acknowledgmentPrefix =  $params->get('AcknowledgmentPrefix',"");
+	$acknowledgmentLink =  $params->get('AcknowledgmentLink',"");
+	$acknowledgmentName =  $params->get('AcknowledgmentName',"");
 	require JModuleHelper::getLayoutPath('mod_ukrgbnestcam', $layout);
 }
--- mod_ukrgbnestcam/tmpl/default.php
+++ mod_ukrgbnestcam/tmpl/default.php
@@ -2,5 +2,6 @@
 // No direct access
 defined('_JEXEC') or die; ?>
 <div class="flex-video widescreen">
-<iframe allowfullscreen src="<?php echo $url ?>" frameborder="0" width="1024" height="576"></iframe>
+	<iframe allowfullscreen src="<?php echo $url ?>" frameborder="0" width="1024" height="576"></iframe>
 </div>
+<p><?php echo $acknowledgmentPrefix?> <a href="<?php echo $acknowledgmentLink?>"><?php echo $acknowledgmentName?></a></p>
